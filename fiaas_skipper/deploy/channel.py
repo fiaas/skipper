e010725e858b1cf9ae08414c414572864822f562
birgirst <birgir.stefansson@schibsted.com>
2018-02-28 14:25:35 +0100

    Read s3bucket reference from config instead of environment

--- fiaas_skipper/deploy/channel.py
+++ fiaas_skipper/deploy/channel.py
@@ -1,5 +1,4 @@
 import json
-import os
 
 import boto3
 
@@ -12,9 +11,12 @@ class ReleaseChannel(object):
 
 
 class ReleaseChannelFactory(object):
+    def __init__(self, config):
+        self.s3bucket = config.s3bucket
+
     def __call__(self, name, tag):
         s3 = boto3.resource('s3')
-        content_object = s3.Object(os.environ['S3BUCKET'], '%s/%s.json' % (name, tag))
+        content_object = s3.Object(self.s3bucket, '%s/%s.json' % (name, tag))
         file_content = content_object.get()['Body'].read().decode('utf-8')
         metadata = json.loads(file_content)
         return ReleaseChannel(name, tag, metadata['image'])
