147be8ffcd04208a626043c01751bad39a8d8a2b
birgirst <birgir.stefansson@schibsted.com>
2018-03-15 15:59:56 +0100

    Generate coverage and report to codacy

--- setup.py
+++ setup.py
@@ -43,6 +43,11 @@ TESTS_REQ = [
     'pytest >= 3.0',
 ]
 
+CI_REQ = [
+    'tox==2.9.1',
+    'tox-travis',
+]
+
 setup(
     name="fiaas-skipper",
     url="https://github.com/fiaas/skipper",
@@ -55,8 +60,9 @@ setup(
     install_requires=GENERIC_REQ + WEB_REQ,
     setup_requires=['pytest-runner', 'wheel', 'setuptools_scm'],
     extras_require={
-        "dev": TESTS_REQ + CODE_QUALITY_REQ,
-        "ci": ["tox==2.9.1", "tox-travis"],
+        "dev": TESTS_REQ + CODE_QUALITY_REQ + CI_REQ,
+        "ci": CI_REQ,
+        "codacy": ["codacy-coverage"],
     },
     tests_require=TESTS_REQ,
     entry_points={"console_scripts": ['skipper=fiaas_skipper:main']},
