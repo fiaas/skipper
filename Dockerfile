0e4d678282fda7c229f525399c7f82fddb681e91
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2018-05-24 10:27:42 +0200

    Run with uwsgi

--- Dockerfile
+++ Dockerfile
@@ -8,15 +8,21 @@ FROM common as build
 # Install build tools, and build wheels of all dependencies
 RUN apk --no-cache add \
     build-base \
+    linux-headers \
     git \
     yaml-dev
 COPY . /skipper
 WORKDIR /skipper
 RUN pip wheel . --wheel-dir=/wheels/
+RUN pip wheel uwsgi==2.0.17 --wheel-dir=/wheels/
 
 FROM common as production
 # Get rid of all build dependencies, install application using only pre-built binary wheels
 COPY --from=build /wheels/ /wheels/
-RUN pip install --no-index --find-links=/wheels/ --only-binary all /wheels/fiaas_skipper*.whl
+RUN pip install --no-index --find-links=/wheels/ --only-binary all /wheels/fiaas_skipper*.whl && \
+    pip install --no-index --find-links=/wheels/ --only-binary all uwsgi
+
+
+
 EXPOSE 5000
-CMD ["skipper"]
+CMD ["/usr/local/bin/uwsgi", "--die-on-term", "--http", "0.0.0.0:5000", "--wsgi", "fiaas_skipper.wsgi:app"]
--- fiaas_skipper/__init__.py
+++ fiaas_skipper/__init__.py
@@ -17,16 +17,6 @@ from .logsetup import init_logging
 from .web import create_webapp
 
 
-class Main(object):
-    def __init__(self, webapp, config):
-        self._webapp = webapp
-        self._config = config
-
-    def run(self):
-        # Run web-app in main thread
-        self._webapp.run("0.0.0.0", self._config.port)
-
-
 def init_k8s_client(config):
     k8s_config.api_server = config.api_server
     k8s_config.api_token = config.api_token
@@ -45,37 +35,40 @@ def _load_spec_config(spec_file):
         return yaml.safe_load(stream)
 
 
-def main():
+def create_app():
     cfg = Configuration()
     init_logging(cfg)
     init_k8s_client(cfg)
     log = logging.getLogger(__name__)
-    try:
-        log.info("fiaas-skipper starting with configuration {!r}".format(cfg))
-        cluster = Cluster()
-        if cfg.release_channel_metadata:
-            log.debug("!!Using hardcoded release channel metadata {!r}".format(cfg.release_channel_metadata))
-            release_channel_factory = FakeReleaseChannelFactory(json.loads(cfg.release_channel_metadata))
-        else:
-            release_channel_factory = ReleaseChannelFactory(cfg.baseurl)
-        spec_config = None
-        if os.path.isfile(cfg.spec_file):
-            try:
-                spec_config = _load_spec_config(cfg.spec_file)
-                log.debug("Loaded spec config from file {!r}".format(cfg.spec_file))
-            except yaml.YAMLError:
-                log.exception("Unable to load spec config file {!r} using defaults".format(cfg.spec_file))
-        if cfg.enable_crd_support:
-            deployer = CrdDeployer(cluster=cluster, release_channel_factory=release_channel_factory,
-                                   bootstrap=CrdBootstrapper(), spec_config=spec_config)
-        elif cfg.enable_tpr_support:
-            deployer = TprDeployer(cluster=cluster, release_channel_factory=release_channel_factory,
-                                   bootstrap=TprBootstrapper(), spec_config=spec_config)
-        webapp = create_webapp(deployer, cluster)
-        Main(webapp=webapp, config=cfg).run()
-    except BaseException:
-        log.exception("General failure! Inspect traceback and make the code better!")
+    log.info("fiaas-skipper starting with configuration {!r}".format(cfg))
+    cluster = Cluster()
+    if cfg.release_channel_metadata:
+        log.debug("!!Using hardcoded release channel metadata {!r}".format(cfg.release_channel_metadata))
+        release_channel_factory = FakeReleaseChannelFactory(json.loads(cfg.release_channel_metadata))
+    else:
+        release_channel_factory = ReleaseChannelFactory(cfg.baseurl)
+    spec_config = None
+    if os.path.isfile(cfg.spec_file):
+        try:
+            spec_config = _load_spec_config(cfg.spec_file)
+            log.debug("Loaded spec config from file {!r}".format(cfg.spec_file))
+        except yaml.YAMLError:
+            log.exception("Unable to load spec config file {!r} using defaults".format(cfg.spec_file))
+    if cfg.enable_crd_support:
+        deployer = CrdDeployer(cluster=cluster, release_channel_factory=release_channel_factory,
+                               bootstrap=CrdBootstrapper(), spec_config=spec_config)
+    elif cfg.enable_tpr_support:
+        deployer = TprDeployer(cluster=cluster, release_channel_factory=release_channel_factory,
+                               bootstrap=TprBootstrapper(), spec_config=spec_config)
+    else:
+        raise RuntimeError("Invalid configuration: Either --enable-tpr-support or --enable-crd-support must be set")
+    return create_webapp(deployer, cluster), cfg
 
 
 if __name__ == "__main__":
-    main()
+    log = logging.getLogger(__name__)
+    app, cfg = create_app()
+    try:
+        app.run("0.0.0.0", cfg.port)
+    except BaseException:
+        log.exception("General failure! Inspect traceback and make the code better!")
