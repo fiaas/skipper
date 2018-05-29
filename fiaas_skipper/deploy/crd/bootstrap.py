7775421d60f1a8e0e8c6d6a57c03ae41db0efe34
Birgir Stefansson <birgir@gmail.com>
2018-05-29 02:38:31 +0200

    fixup! Add bare pod bootstrapper

--- fiaas_skipper/deploy/crd/bootstrap.py
+++ fiaas_skipper/deploy/crd/bootstrap.py
@@ -8,12 +8,12 @@ from k8s.models.common import ObjectMeta
 from k8s.models.custom_resource_definition import CustomResourceDefinition, CustomResourceDefinitionSpec, \
     CustomResourceDefinitionNames
 
-from ...deploy.bootstrap import Bootstrapper
+from ...deploy.bootstrap import BatchJobsBootstrapper
 
 LOG = logging.getLogger(__name__)
 
 
-class CrdBootstrapper(Bootstrapper):
+class CrdBootstrapper(BatchJobsBootstrapper):
     def __init__(self):
         def _create(kind, plural, short_names, group):
             name = "%s.%s" % (plural, group)
