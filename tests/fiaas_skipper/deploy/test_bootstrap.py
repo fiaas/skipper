0340e76c5a6a2e0312bc6bfd9ed0d3cbb885c73e
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2018-06-14 13:39:33 +0200

    DOCD-1685 Add more testcases

--- tests/fiaas_skipper/deploy/test_bootstrap.py
+++ tests/fiaas_skipper/deploy/test_bootstrap.py
@@ -1,12 +1,30 @@
 from __future__ import absolute_import
 
 import copy
+import mock
 
+from k8s.models.resourcequota import ResourceQuota, ResourceQuotaSpec,  NotBestEffort, BestEffort
+from k8s.models.common import ObjectMeta
 import pytest
 
 from fiaas_skipper.deploy.deploy import DeploymentConfig, default_spec_config
 from fiaas_skipper.deploy.channel import ReleaseChannel
 from fiaas_skipper.deploy.bootstrap import BarePodBootstrapper
+
+
+ONLY_BEST_EFFORT_ALLOWED = {
+    "hard": {
+        "pods": "0",
+    },
+    "scopes": [NotBestEffort],
+}
+
+BEST_EFFORT_NOT_ALLOWED = {
+    "hard": {
+        "pods": "0",
+    },
+    "scopes": [BestEffort],
+}
 
 
 class TestBarePodBootstrapper():
@@ -18,14 +36,45 @@ class TestBarePodBootstrapper():
     def spec_config(self):
         return copy.deepcopy(default_spec_config)
 
-    @pytest.mark.parametrize("namespace,resourcequotas,resources", [
-        ("default", [], {
+    @pytest.fixture
+    def resourcequota_list(self):
+        with mock.patch("k8s.models.resourcequota.ResourceQuota.list") as mockk:
+            yield mockk
+
+    def create_resourcequota(self, namespace, resourcequota_spec):
+        metadata = ObjectMeta(name="foo", namespace=namespace)
+        spec = ResourceQuotaSpec.from_dict(resourcequota_spec)
+        return ResourceQuota(metadata=metadata, spec=spec)
+
+
+
+    @pytest.mark.parametrize("namespace,resourcequota_spec,resources", [
+        ("default", None, {
+            'requests': {
+                'memory': '256Mi',
+            },
+        }),
+        ("other-namespace", None, {
+            'requests': {
+                'memory': '256Mi',
+            },
+        }),
+        ("default", ONLY_BEST_EFFORT_ALLOWED, {}),
+        ("other-namespace", ONLY_BEST_EFFORT_ALLOWED, None),
+        ("default", BEST_EFFORT_NOT_ALLOWED, {
+            'requests': {
+                'memory': '256Mi',
+            },
+        }),
+        ("other-namespace", BEST_EFFORT_NOT_ALLOWED, {
             'requests': {
                 'memory': '256Mi',
             },
         }),
     ])
-    def test_bootstrap(self, post, delete, namespace, resourcequotas, resources, spec_config):
+    def test_bootstrap(self, post, delete, resourcequota_list, namespace, resourcequota_spec, resources, spec_config):
+        resourcequota_list.return_value = \
+            [self.create_resourcequota(namespace, resourcequota_spec)] if resourcequota_spec else []
         bootstrapper = BarePodBootstrapper()
         channel = ReleaseChannel(None, None, {'image': 'example.com/image:tag'})
         deployment_config = DeploymentConfig('foo', namespace, 'latest')
@@ -47,7 +96,6 @@ class TestBarePodBootstrapper():
                     'volumeMounts': [],
                     'imagePullPolicy': 'IfNotPresent',
                     'command': ['fiaas-deploy-daemon-bootstrap'],
-                    'resources': resources,
                 }],
                 'restartPolicy': 'Never',
                 'dnsPolicy': 'ClusterFirst',
@@ -56,6 +104,8 @@ class TestBarePodBootstrapper():
                 'initContainers': [],
             }
         }
+        if resources:
+            expected_pod['spec']['containers'][0]['resources'] = resources
 
         bootstrapper(deployment_config, channel, spec_config=spec_config)
 
