b65a169e6316f3029a5c5a3a8ca6f95a9a25866a
Birgir Stefansson <birgir@gmail.com>
2018-06-27 02:21:49 +0200

    Fix indentation

--- fiaas_skipper/web/static/skipper.js
+++ fiaas_skipper/web/static/skipper.js
@@ -1,58 +1,58 @@
-  function getStatus() {
+function getStatus() {
+  $.ajax({
+    type: "GET",
+    url: "/api/status",
+    success: function(data) {
+      var json = $.parseJSON(data);
+      console.log(json);
+      var statusmap = {
+       "SUCCESS": "success",
+       "UNKNOWN": "warning",
+       "UNAVAILABLE": "warning",
+       "ERROR": "danger"
+      };
+      $("#tbody").empty();
+      $.each(json, function (index, item) {
+           var eachrow = "<tr class=\"" + statusmap[item.status] + "\">"
+                       + "<th scope=\"row\">" + item.namespace + "</td>"
+                       + "<td>" + item.status + "</td>"
+                       + "<td>" + [item.description].join('') + "</td>"
+                       + "<td><button class=\"btn btn-primary btn-block btnDeploy\" type=\"submit\" data-namespace=\"" + item.namespace + "\">Deploy</button></td>"
+                       + "</tr>";
+           $('#tbody').append(eachrow);
+       });
+       $(".btnDeploy").each(function(){
+           var $this = $(this);
+           $this.click(function(){
+               $.ajax({
+                   type: "POST",
+                   contentType: "application/json",
+                   url: "/api/deploy",
+                   data: "{\"namespaces\": [\"" + $(this).data('namespace') + "\"]}",
+                   success: function(data) {
+                       alert("Deployment to " + $this.data('namespace') + " performed successfully");
+                   }
+               });
+           });
+       });
+    }
+  });
+  window.setTimeout(function() { getStatus() }, 180000);
+}
+
+function onStatusPageLoad() {
+  console.log( "starting periodically fetching status of namespaces" );
+  getStatus();
+}
+
+function onDeployPageLoad() {
+  $("#btnSubmit").click(function(){
     $.ajax({
-      type: "GET",
-      url: "/api/status",
+      type: "POST",
+      url: "/api/deploy",
       success: function(data) {
-        var json = $.parseJSON(data);
-        console.log(json);
-        var statusmap = {
-         "SUCCESS": "success",
-         "UNKNOWN": "warning",
-         "UNAVAILABLE": "warning",
-         "ERROR": "danger"
-        };
-        $("#tbody").empty();
-        $.each(json, function (index, item) {
-             var eachrow = "<tr class=\"" + statusmap[item.status] + "\">"
-                         + "<th scope=\"row\">" + item.namespace + "</td>"
-                         + "<td>" + item.status + "</td>"
-                         + "<td>" + [item.description].join('') + "</td>"
-                         + "<td><button class=\"btn btn-primary btn-block btnDeploy\" type=\"submit\" data-namespace=\"" + item.namespace + "\">Deploy</button></td>"
-                         + "</tr>";
-             $('#tbody').append(eachrow);
-         });
-         $(".btnDeploy").each(function(){
-             var $this = $(this);
-             $this.click(function(){
-                 $.ajax({
-                     type: "POST",
-                     contentType: "application/json",
-                     url: "/api/deploy",
-                     data: "{\"namespaces\": [\"" + $(this).data('namespace') + "\"]}",
-                     success: function(data) {
-                         alert("Deployment to " + $this.data('namespace') + " performed successfully");
-                     }
-                 });
-             });
-         });
+        alert("Deployment performed successfully");
       }
     });
-    window.setTimeout(function() { getStatus() }, 180000);
-  }
-
-  function onStatusPageLoad() {
-    console.log( "starting periodically fetching status of namespaces" );
-    getStatus();
-  }
-
-    function onDeployPageLoad() {
-      $("#btnSubmit").click(function(){
-        $.ajax({
-          type: "POST",
-          url: "/api/deploy",
-          success: function(data) {
-            alert("Deployment performed successfully");
-          }
-        });
-      });
-    }
+  });
+}
