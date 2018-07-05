9a37dbc7902d1d853ba46a91581e3ad45ba37d52
Morten Lied Johansen <morten.lied.johansen@finn.no>
2018-07-05 15:33:05 +0200

    Wait 30 seconds between each namespace when deploying

--- tests/fiaas_skipper/deploy/test_deploy.py
+++ tests/fiaas_skipper/deploy/test_deploy.py
@@ -22,13 +22,13 @@ class TestDeployer(object):
         return mock.MagicMock(name="release_channel_factory")
 
     def test_deploys_only_to_configured_namespaces(self, cluster, release_channel_factory):
-        deployer = Deployer(cluster, release_channel_factory, None)
+        deployer = Deployer(cluster, release_channel_factory, None, deploy_interval=0)
         deployer._deploy = mock.MagicMock(name="_deploy")
         deployer.deploy(namespaces=("test1",))
         deployer._deploy.assert_called_once()
 
     def test_deploys_to_all_namespaces(self, cluster, release_channel_factory):
-        deployer = Deployer(cluster, release_channel_factory, None)
+        deployer = Deployer(cluster, release_channel_factory, None, deploy_interval=0)
         deployer._deploy = mock.MagicMock(name="_deploy")
         deployer.deploy()
         assert 2 == deployer._deploy.call_count
