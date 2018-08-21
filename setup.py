d2ddc89fb3c459de3bff1b9e753d12cf24cde9b3
Birgir Stefansson <birgir@gmail.com>
2018-08-21 10:20:17 +0200

    Pin version of flask bootstrap

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ WEB_REQ = [
     "Flask == 1.0.2",
     "flask-talisman==0.5.1",
     "blinker == 1.4",
-    "flask-bootstrap",
+    "flask-bootstrap == 3.3.7.1",
     "flask-nav",
     "dominate"
 ]
