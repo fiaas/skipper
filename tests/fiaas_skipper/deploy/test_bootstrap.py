2f4b620d2182b17d7404455c1860ce104ff66235
Jamie Wiebe <jamiewiebe@gmail.com>
2021-09-06 13:00:36 +0200

    fix service account name in tests

--- tests/fiaas_skipper/deploy/test_bootstrap.py
+++ tests/fiaas_skipper/deploy/test_bootstrap.py
@@ -1,12 +1,12 @@
 
 # Copyright 2017-2019 The FIAAS Authors
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -115,7 +115,7 @@ class TestBarePodBootstrapper():
                 }],
                 'restartPolicy': 'Never',
                 'dnsPolicy': 'ClusterFirst',
-                'serviceAccountName': 'default',
+                'serviceAccountName': 'fiaas-deploy-daemon',
                 'imagePullSecrets': [],
                 'initContainers': [],
             }
